4a37103e838b4b7c63824e3e0b9dd41b588a429f
ItsMeAndrea <itsandrea_94@hotmail.com>
2020-11-17 00:38:02 -0400

    setup navegacion

--- preguntadosFront/src/screens/LoginScreen.js
+++ preguntadosFront/src/screens/LoginScreen.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import api from "../api/api";
 
 const LoginScreen = () => {
+  const getusers = async () => {
+    const response = await api.get("/users");
+    console.log(response.data);
+  };
+
+  getusers();
+
   return (
     <View>
       <Text>Login Screen</Text>
